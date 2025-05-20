7b4507b7be04b20f2f6caceeabcbeedc5d78e359
jubeormk1 <jubeormk1@gmail.com>
2025-05-20 14:04:37 +1000

    WIP: Making a functional std server example
    Looks like many things will need to change

--- async/examples/serv1.rs
+++ async/examples/serv1.rs
@@ -4,53 +4,51 @@ use {
     log::{debug, error, info, log, trace, warn},
 };
 
-use std::{net::Ipv6Addr, io::Read};
 use std::path::Path;
+use std::sync::mpsc;
+use std::{io::Read, net::Ipv6Addr};
 
-use anyhow::{Context, Result, Error, bail};
+use anyhow::{bail, Context, Error, Result};
 use embassy_sync::blocking_mutex::raw::{NoopRawMutex, RawMutex};
 use embassy_sync::mutex::Mutex;
 use pretty_hex::PrettyHex;
 
-use tokio::net::{TcpStream, TcpListener};
-use tokio::io::{AsyncReadExt,AsyncWriteExt};
-use tokio::sync::oneshot;
-use tokio::runtime::Runtime;
-use tokio::task::spawn_local;
-
 use embedded_io_adapters::tokio_1::FromTokio;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::runtime::Runtime;
+use tokio::sync::oneshot;
+use tokio::task;
+use tokio::task::spawn_local;
 
 use sunset::*;
 use sunset_embassy::SSHServer;
 
-// struct StdRawMutex {
-//     l: std::sync::Mutex<()>,
-// }
+struct StdRawMutex {
+    l: std::sync::Mutex<()>,
+}
 
-// unsafe impl RawMutex for StdRawMutex {
-//     const INIT: Self = Self::new();
+unsafe impl RawMutex for StdRawMutex {
+    const INIT: Self = Self::new();
 
-//     fn lock<R>(&self, f: impl FnOnce() -> R) -> R {
-//         let x = self.l.lock();
-//         f()
-//     }
-// }
+    fn lock<R>(&self, f: impl FnOnce() -> R) -> R {
+        let x = self.l.lock();
+        f()
+    }
+}
 
-// impl StdRawMutex {
-//     const fn new() -> Self {
-//         Self {
-//             l: std::sync::Mutex::new(()),
-//         }
-//     }
-// }
-
+impl StdRawMutex {
+    const fn new() -> Self {
+        Self { l: std::sync::Mutex::new(()) }
+    }
+}
 
 use simplelog::*;
 #[derive(argh::FromArgs)]
 /** con1
  */
 struct Args {
-    #[argh(switch, short='v')]
+    #[argh(switch, short = 'v')]
     /// verbose debug logging
     debug: bool,
 
@@ -58,14 +56,13 @@ struct Args {
     /// more verbose
     trace: bool,
 
-    #[argh(option, short='p', default="2244")]
+    #[argh(option, short = 'p', default = "2244")]
     /// port
     port: u16,
 
     #[argh(option)]
     /// a path to hostkeys. At most one of each key type.
     hostkey: Vec<String>,
-
 }
 
 fn parse_args() -> Result<Args> {
@@ -81,15 +78,12 @@ fn main() -> Result<()> {
 
     if args.hostkey.is_empty() {
         error!("At least one --hostkey is required");
-        return Ok(())
+        return Ok(());
     }
 
-    let rt  = Runtime::new().unwrap();
+    let rt = Runtime::new().unwrap();
     let local = task::LocalSet::new();
-    local.block_on(&rt, async {
-        run(&args).await
-    })
-    .map_err(|e| {
+    local.block_on(&rt, async { run(&args).await }).map_err(|e| {
         error!("Exit with error: {e:?}");
         e
     })
@@ -98,14 +92,15 @@ fn main() -> Result<()> {
 fn setup_log(args: &Args) -> Result<()> {
     let mut conf = simplelog::ConfigBuilder::new();
     let conf = conf
-    .add_filter_allow_str("sunset")
-    .add_filter_allow_str("serv1")
-    // not debugging these bits of the stack at present
-    .add_filter_ignore_str("sunset::traffic")
-    .add_filter_ignore_str("sunset::runner")
-    .add_filter_ignore_str("sunset_async::async_sunset")
-    .set_time_offset_to_local().expect("Couldn't get local timezone")
-    .build();
+        .add_filter_allow_str("sunset")
+        .add_filter_allow_str("serv1")
+        // not debugging these bits of the stack at present
+        .add_filter_ignore_str("sunset::traffic")
+        .add_filter_ignore_str("sunset::runner")
+        .add_filter_ignore_str("sunset_async::async_sunset")
+        .set_time_offset_to_local()
+        .expect("Couldn't get local timezone")
+        .build();
 
     let level = if args.trace {
         LevelFilter::Trace
@@ -115,9 +110,12 @@ fn setup_log(args: &Args) -> Result<()> {
         LevelFilter::Warn
     };
 
-    let mut logs: Vec<Box<dyn SharedLogger>> = vec![
-        TermLogger::new(level, conf.clone(), TerminalMode::Mixed, ColorChoice::Auto),
-    ];
+    let mut logs: Vec<Box<dyn SharedLogger>> = vec![TermLogger::new(
+        level,
+        conf.clone(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )];
 
     CombinedLogger::init(logs).unwrap();
     Ok(())
@@ -139,21 +137,17 @@ struct DemoServer<'a> {
 
 impl<'a> DemoServer<'a> {
     fn new(shell: &'a DemoShell, keyfiles: &[String]) -> Result<Self> {
-        let keys = keyfiles.iter().map(|f| {
-            read_key(f).with_context(|| format!("loading key {f}"))
-        }).collect::<Result<Vec<SignKey>>>()?;
+        let keys = keyfiles
+            .iter()
+            .map(|f| read_key(f).with_context(|| format!("loading key {f}")))
+            .collect::<Result<Vec<SignKey>>>()?;
 
-        Ok(Self {
-            sess: None,
-            keys,
-            shell_started: false,
-            shell,
-        })
+        Ok(Self { sess: None, keys, shell_started: false, shell })
     }
 }
 
 impl<'a> ServBehaviour for DemoServer<'a> {
-    fn hostkeys(&mut self) -> BhResult<&[SignKey]> {
+    fn hostkeys(&mut self) -> Result<&[SignKey]> {
         Ok(&self.keys)
     }
 
@@ -166,12 +160,13 @@ impl<'a> ServBehaviour for DemoServer<'a> {
     }
 
     fn auth_password(&mut self, user: TextString, password: TextString) -> bool {
-        user.as_str().unwrap_or("") == "matt" && password.as_str().unwrap_or("") == "pw"
+        user.as_str().unwrap_or("") == "matt"
+            && password.as_str().unwrap_or("") == "pw"
     }
 
     fn auth_pubkey(&mut self, user: TextString, pubkey: &PubKey) -> bool {
         if user.as_str().unwrap_or("") != "matt" {
-            return false
+            return false;
         }
 
         // key is tested1
@@ -209,15 +204,16 @@ struct DemoShell {
 }
 
 impl DemoShell {
-    async fn run<'f, S: ServBehaviour>(self, serv: &SSHServer<'_, S>) -> Result<()>
-    {
+    async fn run<'f>(self, serv: &SSHServer<'_>) -> Result<()> {
+        // async fn run<'f>(self, serv: &SSHServer<'_, S>) -> Result<()> {
+        // async fn run<'f, S: ServBehaviour>(self, serv: &SSHServer<'_, S>) -> Result<()> {
         let session = async {
             // wait for a shell to start
             let chan = if let Ok(c) = self.notify.await {
                 c
             } else {
                 // no shell was started. that's OK.
-                return Ok(())
+                return Ok(());
             };
 
             loop {
@@ -243,8 +239,8 @@ fn run_session(args: &Args, mut stream: TcpStream) -> Result<()> {
 
     spawn_local(async move {
         // TODO: simplify
-        let app = DemoServer::new(&shell, &args.hostkey)?;
-        let app = Mutex::<NoopRawMutex, _>::new(app);
+        // let app = DemoServer::new(&shell, &args.hostkey)?;
+        // let app = Mutex::<NoopRawMutex, _>::new(app);
 
         let mut rxbuf = vec![0; 3000];
         let mut txbuf = vec![0; 3000];
@@ -255,7 +251,7 @@ fn run_session(args: &Args, mut stream: TcpStream) -> Result<()> {
             let (rsock, wsock) = stream.split();
             let mut rsock = FromTokio::new(rsock);
             let mut wsock = FromTokio::new(wsock);
-            serv.run(&mut rsock, &mut wsock, &app).await
+            serv.run(&mut rsock, &mut wsock).await
         });
 
         spawn_local(shell.run(serv));
@@ -266,7 +262,8 @@ fn run_session(args: &Args, mut stream: TcpStream) -> Result<()> {
 
 async fn run(args: &Args) -> Result<()> {
     // TODO not localhost. also ipv6?
-    let listener = TcpListener::bind(("127.6.6.6", args.port)).await.context("Listening")?;
+    let listener =
+        TcpListener::bind(("127.6.6.6", args.port)).await.context("Listening")?;
     loop {
         let (stream, _) = listener.accept().await?;
 
